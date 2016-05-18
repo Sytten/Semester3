da8795b3a0917d03af306fadc377977ecf259696
Emile Fugulin <emilefugulin@hotmail.com>
2016-05-18 16:08:01 -0400

    Define types of Terminal Expression

--- src/app2/Terminal.java
+++ src/app2/Terminal.java
@@ -2,20 +2,39 @@ package app2;
 
 /** @author Ahmed Khoumsi */
 
-/** Cette classe identifie les terminaux reconnus et retournes par
- *  l'analyseur lexical
+/**
+ * Cette classe identifie les terminaux reconnus et retournes par l'analyseur
+ * lexical
  */
 public class Terminal {
 
-// Constantes et attributs
-  public String chaine;
+	/**
+	 * Enumeration du type de terminal
+	 */
+	public enum Type {
+		OPERATEUR, CONSTANTE, VARIABLE
+	}
 
+	/**
+	 * Variables membres
+	 */
+	private String mChaine;
+	private Type mType;
 
-/** Un ou deux constructeurs (ou plus, si vous voulez)
-  *   pour l'initalisation d'attributs
- */
-  public Terminal(String s) {   // arguments possibles
-	  chaine = s;
-  }
-
+	/**
+	 * Un ou deux constructeurs (ou plus, si vous voulez) pour l'initalisation
+	 * d'attributs
+	 */
+	public Terminal(String pChaine, Type pType) { // arguments possibles
+		mChaine = pChaine;
+		mType = pType;
+	}
+	
+	public Type getType() {
+		return mType;
+	}
+	
+	public String getChaine() {
+		return mChaine;
+	}
 }
